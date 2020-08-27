b0d7f489aa4908f600ffbd2c0c41836e34ef943a
jobairRafi96 <jubairrafi96960@gmail.com>
2020-08-27 09:56:51 -0700

    article created and showing

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require("express")
+const mongoose =require("mongoose")
+const Article = require("./models/article")
+
 const articleRouter = require("./routes/articles") //require from routes folder
 const app = express()
 
+mongoose.connect("mongodb://localhost/blog",{useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false})
 
 app.set('view engine', 'ejs')
 
-
+app.use(express.urlencoded({ extended: false}))
 app.use("/articles", articleRouter) //using router
 
-app.get("/",(req,res)=>{
-  const articles=[{
-    title: "Test article",
-    createdAt: new Date(),
-    description: "Test description"
-  }]
-  res.render("index",{articles: articles})
+app.get("/", async(req,res)=>{
+  const articles = await Article.find().sort({
+    createdAt: "desc"
+  })
+  res.render("articles/index",{articles: articles})
 })
 
 app.listen(3000)
--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,30 @@
 const express = require("express")
+const Article = require("./../models/article")
 const router = express.Router() //for routing
 
-router.get("/",(req,res)=>{
-  res.send("this is routes")
+
+router.get("/new",  (req,res)=>{  //same as ejs layout
+  res.render("articles/new", {article: new Article()})
+})
+
+router.get("/:id", async (req, res)=>{
+  const article = await Article.findById(req.params.id)
+  if(article == null) res.redirect("/")
+  res.render("articles/show",{ article: article})
+})
+
+router.post("/", async (req,res)=>{
+  let article = new Article({
+    title : req.body.title,
+    description : req.body.description,
+    markdown : req.body.markdown
+  })
+
+  try{
+    article = await article.save()
+    res.redirect(`/articles/${article.id}`)
+  }catch(err){
+    res.render("articles/new",{article:article})
+  }
 })
 module.exports = router
